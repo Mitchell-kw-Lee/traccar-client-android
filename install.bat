b60d031be060073639b7219f06be544e5e8abc78
Mitchell Lee <mitchell.kw.lee@gmail.com>
2018-11-27 12:25:01 +0900

    Utility batch for installing/uninstalling

---
+++ install.bat
@@ -0,0 +1,3 @@
+"C:\Users\Mitchell Lee\AppData\Local\Android\Sdk\platform-tools\adb" install -r "C:\traccar-client-android-master\app-regular-release.apk"
+
+timeout 10
---
+++ uninstall.bat
@@ -0,0 +1,3 @@
+"C:\Users\Mitchell Lee\AppData\Local\Android\Sdk\platform-tools\adb" uninstall org.traccar.client
+
+timeout 10
